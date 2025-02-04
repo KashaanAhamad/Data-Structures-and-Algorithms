8abcefff842db38d4c6e967e971cf34fc19b932a
Kashaan Ahamad <kashaan.bryan@gmail.com>
2025-02-05 02:25:31 +0530

    maximum Ascendin SubArray Sum

---
+++ ArraysBasics/maximunAscendingSubarraySum.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+// Compile it using g++ -o main.exe <File_Relative_Path>
+
+int AscendingSubarraySum(int arr[], int n)
+{
+    int sum=arr[0];
+    int maxno=0;
+    for(int i=1; i<n;i++){
+        
+        if(arr[i]>arr[i-1]){
+            sum+=arr[i];
+           
+        }
+        else{
+             maxno=max(maxno,sum);
+            sum=arr[i];
+            
+        }
+        
+        
+
+
+    }
+    return max(sum,maxno);
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    int arr[] = {12,17,15,13,10,11,12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    
+    cout<<AscendingSubarraySum(arr, n);
+
+     
+    return 0;
+}

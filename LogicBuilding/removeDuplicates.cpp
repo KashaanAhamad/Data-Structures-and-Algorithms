56103e411c4e5832ab13b97443cfe6ca1b7990cd
Kashaan Ahmad <kashaan.bryan@gmail.com>
2026-03-25 23:48:55 +0530

    Removing Duplicates from the array

---
+++ LogicBuilding/removeDuplicates.cpp
@@ -0,0 +1,18 @@
+//Leetcode Problem No. 26
+//Remove Duplicates from Sorted Array
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int k=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+
+        }
+        return k;
+    }
+};
